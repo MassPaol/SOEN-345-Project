be093c1f4c457806e8f8dccf682dd53f6560b8d6
Massimo Paolini <mpaolini119@gmail.com>
2026-02-11 23:51:09 -0500

    Built the Registration UI, closes #9

---
+++ app/src/main/java/com/team_one/soen_345_project/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.team_one.soen_345_project.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.team_one.soen_345_project.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
