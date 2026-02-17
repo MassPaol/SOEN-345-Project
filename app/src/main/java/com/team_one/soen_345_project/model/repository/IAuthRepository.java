518eeaeee4286694e9004150655e6c9dcb96c445
Ammar Ranko <ammar.ranko707@gmail.com>
2026-02-16 21:43:16 -0500

    Add loginUser method to IAuthRepository interface

--- app/src/main/java/com/team_one/soen_345_project/model/repository/IAuthRepository.java
+++ app/src/main/java/com/team_one/soen_345_project/model/repository/IAuthRepository.java
@@ -3,4 +3,5 @@ package com.team_one.soen_345_project.model.repository;
 import com.team_one.soen_345_project.model.util.Callback;
 public interface IAuthRepository {
     void createUser(String[] registrationFields, Callback callback);
+    void loginUser(String email, String password, Callback callback);
 }
